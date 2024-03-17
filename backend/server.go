0078dd1851c3b338a020dbc0e56a6d5bfd5e8198
jwt2706 <jwt2home@gmail.com>
2024-03-17 19:53:51 -0400

    BE: setup

---
+++ backend/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type PasswordResponse struct {
+	Password string `json:"password"`
+}
+
+func generatePassword(length int) string {
+	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>/?`~"
+	var password strings.Builder
+
+	for i := 0; i < length; i++ {
+		password.WriteByte(characters[rand.Intn(len(characters))])
+	}
+
+	return password.String()
+}
+
+func handleGeneratePassword(w http.ResponseWriter, r *http.Request) {
+	length := 10
+	if len(r.URL.Query()["length"]) > 0 {
+		length, _ = strconv.Atoi(r.URL.Query()["length"][0])
+	}
+
+	password := generatePassword(length)
+	response := PasswordResponse{Password: password}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	http.HandleFunc("/generate-password", handleGeneratePassword)
+	http.ListenAndServe(":8080", nil)
+}
