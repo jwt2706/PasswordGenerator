7c240946853772976507d62e16c0f0110ca99206
jwt2706 <jwt2home@gmail.com>
2024-03-17 21:29:14 -0400

    FE: api request

---
+++ frontend/request.js
@@ -0,0 +1,20 @@
+document.getElementById('password-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  let length = document.getElementById('length').value;
+  
+  fetch('https://https://backend-mauve-delta.vercel.app/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ length: length }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('password').textContent = data.password;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+});
