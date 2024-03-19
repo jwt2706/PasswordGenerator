8418af8934c6184d6e38f394717aa057b488286f
jwt2706 <jwt2home@gmail.com>
2024-03-18 22:33:08 -0400

    BE: run on serverless func

---
+++ backend/api/generate-password.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+    "encoding/json"
+    "net/http"
+    "strconv"
+    "strings"
+    "math/rand"
+    "time"
+)
+
+type PasswordResponse struct {
+    Password string `json:"password"`
+}
+
+func generatePassword(length int) string {
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>/?`~"
+    var password strings.Builder
+
+    for i := 0; i < length; i++ {
+        password.WriteByte(characters[rand.Intn(len(characters))])
+    }
+
+    return password.String()
+}
+
+func enableCors(w *http.ResponseWriter) {
+    (*w).Header().Set("Access-Control-Allow-Origin", "https://jwt2706.ca")
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+    enableCors(&w)
+
+    if r.Method == http.MethodOptions {
+        w.WriteHeader(http.StatusOK)
+        return
+    }
+
+    length := 10
+    if len(r.URL.Query()["length"]) > 0 {
+        length, _ = strconv.Atoi(r.URL.Query()["length"][0])
+    }
+
+    password := generatePassword(length)
+    response := PasswordResponse{Password: password}
+    json.NewEncoder(w).Encode(response)
+}
