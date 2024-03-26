d7ec9dbed0c763cbe55bd640b1114d8914c3e3ec
jwt2706 <jwt2home@gmail.com>
2024-03-26 09:19:35 -0400

    handle post requests

--- api/generate-password.go
+++ api/generate-password.go
@@ -24,19 +24,23 @@ func generatePassword(length int) string {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodOptions {
-        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+    if r.Method == http.MethodOptions {
         w.WriteHeader(http.StatusOK)
         return
-    }
+    } else if r.Method == http.MethodPost {
+        length := 10
+        if len(r.URL.Query()["length"]) > 0 {
+            length, _ = strconv.Atoi(r.URL.Query()["length"][0])
+        }
 
-    length := 10
-    if len(r.URL.Query()["length"]) > 0 {
-        length, _ = strconv.Atoi(r.URL.Query()["length"][0])
+        password := generatePassword(length)
+        response := PasswordResponse{Password: password}
+        json.NewEncoder(w).Encode(response)
+    } else {
+        w.WriteHeader(http.StatusMethodNotAllowed)
     }
-
-    password := generatePassword(length)
-    response := PasswordResponse{Password: password}
-    json.NewEncoder(w).Encode(response)
 }
